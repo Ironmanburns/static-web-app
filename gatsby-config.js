c330a7924fc4abd96b60e3566c7ccf81081cf321
Jason Burns <36903988+Ironmanburns@users.noreply.github.com>
2022-07-01 17:43:25 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: ` Default Starter`,
+    description: `hellow world.`,
+    author: `@ironmanburns`,
+    siteUrl: `https://www.burnsyhosting.co.uk/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
